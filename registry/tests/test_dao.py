3a20f45b2546de40e3649b7717c79257cd948400
Paul Smith <paul.smith@postily.co.uk>
2019-08-17 16:39:49 +0100

    Parameterise test_daos

--- registry/tests/test_dao.py
+++ registry/tests/test_dao.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app import restful
 from registry.dao import Dao
 
@@ -9,14 +11,14 @@ entities_to_test = ['UserType',
                     'Surgery']
 
 
-def test_daos(database_session):
-    for entity_name in entities_to_test:
-        dao = Dao.find_dao(database_session, entity_name)
-        all = dao.find_all()
-        assert len(all) > 0
-        for item in all:
-            assert item == dao.find_id(item.id)
+@pytest.mark.parametrize('entity_name', entities_to_test)
+def test_daos(database_session, entity_name):
+    dao = Dao.find_dao(database_session, entity_name)
+    all = dao.find_all()
+    assert len(all) > 0
+    for item in all:
+        assert item == dao.find_id(item.id)
 
-            new_item = dao.new(entity_name)
-            new_item.from_dict(restful.json_loads(restful.json_dumps(item.as_dict())))
-            assert new_item.as_dict() == item.as_dict()
+        new_item = dao.new(entity_name)
+        new_item.from_dict(restful.json_loads(restful.json_dumps(item.as_dict())))
+        assert new_item.as_dict() == item.as_dict()
