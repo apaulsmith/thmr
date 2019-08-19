d6f063a183853278142b2d1c2bbdd8e63414624b
Paul Smith <paul.smith@postily.co.uk>
2019-08-19 23:06:33 +0100

    Try various fixes to make eb happy

--- application.py
+++ application.py
@@ -7,21 +7,20 @@ from app.session_wrapper import SessionGuard
 from registry.schema import Database
 from registry.tests import data_generator
 
-if __name__ == '__main__':
-    config = thmr_config.Config()
-    database = Database(config.DB_URL)
+config = thmr_config.Config()
+database = Database(config.DB_URL)
 
-    database.drop_all()
-    database.create_all()
+database.drop_all()
+database.create_all()
 
-    with app.app_context():
-        flask.current_app.database = database
-        flask.current_app.json_encoder = CustomJSONEncoder
-        # flask.current_app.json_decoder = CustomJSONDecoder
+with app.app_context():
+    flask.current_app.database = database
+    flask.current_app.json_encoder = CustomJSONEncoder
+    # flask.current_app.json_decoder = CustomJSONDecoder
 
-    with SessionGuard() as guard:
-        data_generator.create_sample_data(guard.session,
-                                          num_users=thmr_config.Config.TEST_NUM_USERS,
-                                          num_patients=thmr_config.Config.TEST_NUM_PATIENTS)
-
-    app.run(debug=True)
+with SessionGuard() as guard:
+    data_generator.create_sample_data(guard.session,
+                                      num_users=thmr_config.Config.TEST_NUM_USERS,
+                                      num_patients=thmr_config.Config.TEST_NUM_PATIENTS)
+application = app
+app.run(debug=True)
