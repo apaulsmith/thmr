4a7cbf9ffaa19173dd4f848678299be29c88ff5a
Paul Smith <paul.smith@postily.co.uk>
2019-08-19 22:21:33 +0100

    Add application.py for eb

---
+++ application.py
@@ -0,0 +1,29 @@
+import argparse
+
+import flask
+
+import config as thmr_config
+from app import app
+from app.restful import CustomJSONEncoder
+from app.session_wrapper import SessionGuard
+from registry.schema import Database
+from registry.tests import data_generator
+
+if __name__ == '__main__':
+    config = thmr_config.Config()
+    database = Database(config.DB_URL)
+
+    database.drop_all()
+    database.create_all()
+
+    with SessionGuard as guard:
+        data_generator.create_sample_data(guard.session,
+                                              num_users=thmr_config.Config.TEST_NUM_USERS,
+                                              num_patients=thmr_config.Config.TEST_NUM_PATIENTS)
+
+    with app.app_context():
+        flask.current_app.database = database
+        flask.current_app.json_encoder = CustomJSONEncoder
+        # flask.current_app.json_decoder = CustomJSONDecoder
+
+    app.run(debug=True)
